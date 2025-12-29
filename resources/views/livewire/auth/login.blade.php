9afd66b7bf6c096a603f27305e146db491ca9992
pololosky <follivios@gmail.com>
2025-12-29 12:40:35 +0000

    petite modif des migrations

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.auth>
-    <div class="flex flex-col gap-6">
+    <div class="flex flex-col gap-6 bg-white text-black">
         <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
 
         <!-- Session Status -->
